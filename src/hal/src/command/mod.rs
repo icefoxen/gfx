e918b14eb560ba9805db7846b70d9e80219a4fe2
Simon Heath <icefoxen@gmail.com>
2018-02-09 17:08:15 -0500

    Initial doc pass for the command, pso and queue HAL modules.
    Also sprinkled in about ten billion 'DOC TODO' comments, so I can
    easily find what has yet to be done.

--- src/hal/src/command/mod.rs
+++ src/hal/src/command/mod.rs
@@ -1,4 +1,4 @@
-//!
+//! DOC TODO
 
 use Backend;
 use queue::capability::Supports;
@@ -19,22 +19,33 @@ use std::borrow::{Cow};
 
 /// Trait indicating how many times a Submit can be submitted.
 pub trait Shot {
-    ///
+    /// DOC TODO
     const FLAGS: CommandBufferFlags;
 }
 /// Indicates a Submit that can only be submitted once.
 pub enum OneShot { }
 impl Shot for OneShot { const FLAGS: CommandBufferFlags = CommandBufferFlags::ONE_TIME_SUBMIT; }
+
 /// Indicates a Submit that can be submitted multiple times.
 pub enum MultiShot { }
 impl Shot for MultiShot { const FLAGS: CommandBufferFlags = CommandBufferFlags::EMPTY; }
 
 /// A trait indicating the level of a command buffer.
 pub trait Level { }
+
 /// Indicates a primary command buffer.
+///
+/// Vulkan describes a primary command buffer as one which can be directly submitted
+/// to a queue, and can execute `Secondary` command buffers.
 pub enum Primary { }
 impl Level for Primary { }
+
 /// Indicates a secondary command buffer.
+///
+/// Vulkan describes a secondary command buffer as one which cannot be directly submitted
+/// to a queue, but can be executed by a primary command buffer.  This allows you to
+/// construct multiple secondary command buffers that do specific things, and compose
+/// them together into primary command buffers.
 pub enum Secondary { }
 impl Level for Secondary { }
 
@@ -50,7 +61,7 @@ unsafe impl<B: Backend, C, S, L> Send for Submit<B, C, S, L> {}
 
 /// A trait representing a command buffer that can be added to a `Submission`.
 pub unsafe trait Submittable<'a, B: Backend, C, L: Level> {
-    ///
+    /// DOC TODO
     unsafe fn into_buffer(self) -> Cow<'a, B::CommandBuffer>;
 }
 
@@ -62,7 +73,7 @@ unsafe impl<'a, B: Backend, C, L: Level> Submittable<'a, B, C, L> for &'a Submit
     unsafe fn into_buffer(self) -> Cow<'a, B::CommandBuffer> { Cow::Borrowed(&self.0) }
 }
 
-/// A convenience for not typing out the full signature of a secondary command buffer.
+/// A convenience type for not typing out the full signature of a secondary command buffer.
 pub type SecondaryCommandBuffer<'a, B: Backend, C, S: Shot = OneShot> = CommandBuffer<'a, B, C, S, Secondary>;
 
 /// Command buffer with compute, graphics and transfer functionality.
@@ -101,7 +112,7 @@ impl<'a, B: Backend, C, S: Shot, L: Level> CommandBuffer<'a, B, C, S, L> {
 }
 
 impl<'a, B: Backend, C, S: Shot> CommandBuffer<'a, B, C, S, Primary> {
-    ///
+    /// DOC TODO
     pub fn execute_commands<I, K>(&mut self, submits: I)
     where
         I: IntoIterator,
